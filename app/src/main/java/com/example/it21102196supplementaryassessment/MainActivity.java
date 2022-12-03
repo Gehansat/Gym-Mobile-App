8151fd447dab75947b570cff1d18ab22b8183ff9
it21102196 <it21102196@my.sliit.lk>
2022-12-03 19:41:00 +0530

    onCreate

--- app/src/main/java/com/example/it21102196supplementaryassessment/MainActivity.java
+++ app/src/main/java/com/example/it21102196supplementaryassessment/MainActivity.java
@@ -3,13 +3,77 @@ package com.example.it21102196supplementaryassessment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.it21102196supplementaryassessment.database.DBHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnadd;
+    EditText addname,addage,addaddress,addcnum;
+    DBHelper dbHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addname = findViewById(R.id.addname);
+        addage = findViewById(R.id.addage);
+        addaddress = findViewById(R.id.addaddress);
+        addcnum = findViewById(R.id.addcnum);
+
+        btnadd = findViewById(R.id.btnadd);
+
+        btnadd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = addname.getText().toString();
+                String age = addage.getText().toString();
+                String address = addaddress.getText().toString();
+                String contactnumber = addcnum.getText().toString();
+
+                long checkinsertData = dbHelper.addinfo(name,age,address,contactnumber);
+                if(name.isEmpty()||age.isEmpty()||address.isEmpty()||contactnumber.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please Enter the Values", Toast.LENGTH_SHORT).show();
+                }else{
+                    long inserted = dbHelper.addinfo(name,age,address,contactnumber);
+
+                    if(inserted > 0){
+                        Toast.makeText(MainActivity.this, "Data Inserted Successfully", Toast.LENGTH_SHORT).show();
+                    }else{
+                        Toast.makeText(MainActivity.this, "Error ! Data not inserted!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
+    }
+
+//    public void saveUser(View view){
+//        String name = addname.getText().toString();
+//        String age = addage.getText().toString();
+//        String address = addaddress.getText().toString();
+//        String contactnumber = addcnum.getText().toString();
+//
+//        DBHelper dbHelper = new DBHelper(this);
+//
+//        if(name.isEmpty()||age.isEmpty()||address.isEmpty()||contactnumber.isEmpty()){
+//            Toast.makeText(this, "Please Enter the Values", Toast.LENGTH_SHORT).show();
+//        }else{
+//            long inserted = dbHelper.addinfo(name,age,address,contactnumber);
+//
+//            if(inserted > 0){
+//                Toast.makeText(this, "Data Inserted Successfully", Toast.LENGTH_SHORT).show();
+//            }else{
+//                Toast.makeText(this, "Error ! Data not inserted!", Toast.LENGTH_SHORT).show();
+//            }
+//        }
 
     }
-}
+
+
+
