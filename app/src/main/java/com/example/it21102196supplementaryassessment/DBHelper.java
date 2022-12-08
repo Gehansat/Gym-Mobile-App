552582c94dc8c71d9fc67ab9b9c7f6eef6a20a7a
it21102196 <it21102196@my.sliit.lk>
2022-12-08 18:29:17 +0530

    DBHelper and MyAdapter complete

--- app/src/main/java/com/example/it21102196supplementaryassessment/DBHelper.java
+++ app/src/main/java/com/example/it21102196supplementaryassessment/DBHelper.java
@@ -1,6 +1,91 @@
 package com.example.it21102196supplementaryassessment;
 
-public class DBHelper {
-    public Boolean insertuserdetails(String name, String age, String email) {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class DBHelper extends SQLiteOpenHelper {
+
+    public static final String DBNAME = "userDB.db";
+
+    DBHelper(Context context) {
+        super(context, "userDB.db", null, 1);
     }
+
+    @Override
+    public void onCreate(SQLiteDatabase DB) {
+        DB.execSQL("create Table userdetails(name TEXT primary key , age TEXT,email TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase DB, int i, int ii) {
+        DB.execSQL("drop Table if exists userdetails");
+    }
+
+    public boolean insertuserdetails(String name, String age, String email) {
+        SQLiteDatabase DB = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("age", age);
+        contentValues.put("email", email);
+        long result = DB.insert("userdetails", null, contentValues);
+        if (result == -1)
+            return false;
+        else
+            return true;
+    }
+
+
+//    public boolean checkExercise(String Exercise) {
+//        SQLiteDatabase DB = this.getWritableDatabase();
+//        Cursor cursor = DB.rawQuery("select * from User where Exercise = ?", new String[]{Exercise});
+//        if (cursor.getCount() > 0) {
+//            return true;
+//        }
+//        else {
+//            return false;
+//        }
+//        }
+
+
+
+
+
+
+    public Cursor getdata() {
+        SQLiteDatabase DB = getWritableDatabase();
+        Cursor e = DB.rawQuery("Select * from userdetails", null);
+        return e;
+    }
+
+
+    public Boolean deleteuserdetails(String na)
+    {
+        SQLiteDatabase userDB = this.getWritableDatabase();
+        Cursor cursor = userDB.rawQuery("Select * from userdetails where name = ?", new String[]{na});
+        if(cursor.getCount()>0)
+        {
+            long result = userDB.delete("userdetails", "name=?", new String[]{na});
+            if(result==-1)
+            {
+                return  false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/it21102196supplementaryassessment/MyAdapter.java
+++ app/src/main/java/com/example/it21102196supplementaryassessment/MyAdapter.java
@@ -1,4 +1,148 @@
 package com.example.it21102196supplementaryassessment;
 
-public class MyAdapter {
+//package com.example.testing;
+//
+//        import android.annotation.SuppressLint;
+//        import android.content.Context;
+//        import android.view.LayoutInflater;
+//        import android.view.View;
+//        import android.view.ViewGroup;
+//        import android.widget.TextView;
+//
+//        import androidx.annotation.NonNull;
+//        import androidx.recyclerview.widget.RecyclerView;
+//
+//        import java.text.BreakIterator;
+//        import java.util.ArrayList;
+//
+//public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+//    private Context context;
+//    private ArrayList Exercise, reps;
+//    DBHelper DB;
+//
+//    public MyAdapter(Context context, ArrayList Exercise, ArrayList reps) {
+//        this.context = context;
+//        this.Exercise = Exercise;
+//        this.reps = reps;
+//
+//    }
+//
+//    @NonNull
+//    @Override
+//    public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View v = LayoutInflater.from(context).inflate(R.layout.userentry,parent,false);
+//        return new MyViewHolder(v);
+//    }
+//
+//    @Override
+//    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
+//        holder.Exercise.setText(String.valueOf(Exercise.get(position)));
+//        holder.reps.setText(String.valueOf(reps.get(position)));
+//       // String Exercises = String.valueOf(Exercise.get(position));
+//    }
+//
+//
+//
+//    @Override
+//    public int getItemCount() {
+//        return Exercise.size();
+//    }
+//
+//    public class MyViewHolder extends RecyclerView.ViewHolder {
+//
+//        TextView Exercise, reps;
+//        public MyViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//            Exercise = itemView.findViewById(R.id.addex);
+//            reps = itemView.findViewById(R.id.addreps);
+//        }
+//    }
+//}
+
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private Context context;
+    private ArrayList name, age,email;
+    DBHelper DB;
+
+
+    public MyAdapter(Context context, ArrayList name, ArrayList age,ArrayList email) {
+        this.context = context;
+        this.name = name;
+        this.age = age;
+        this.email = email;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View otherView = LayoutInflater.from(context).inflate(R.layout.userentry,parent,false);
+        return new MyViewHolder(otherView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
+
+
+        holder.name.setText(String.valueOf(name.get(position)));
+        holder.age.setText(String.valueOf(age.get(position)));
+        holder.email.setText(String.valueOf(email.get(position)));
+
+
+        String Ex = String.valueOf(name.get(position));
+
+        //delete button
+        holder.delete.setOnClickListener(new View.OnClickListener() {
+            DBHelper db = new DBHelper(context);
+            @Override
+            public void onClick(View view) {
+                Boolean deleteAct = db.deleteuserdetails(Ex);
+                if (deleteAct == true) {
+                    Toast.makeText(context, "Deleted Successfully", Toast.LENGTH_SHORT).show();
+                    name.remove(position);
+                    notifyDataSetChanged ();
+                }
+                else
+                    Toast.makeText(context, "Failed to delete", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return name.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name, age,email;
+        Button delete;
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.textname);
+            age = itemView.findViewById(R.id.textage);
+            email = itemView.findViewById(R.id.textemail);
+            delete = (Button) itemView.findViewById(R.id.deleteBtn);
+
+        }
+    }
 }
